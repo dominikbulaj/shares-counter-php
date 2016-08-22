32a6d545cc66992d414f3c9bfc1d19d1cb9945a2
Dominik Bulaj <dominik@bulaj.com>
2016-08-22 14:46:40 +0200

    Fixed getting Facebook shares (API changed location of this element)

--- examples/index.php
+++ examples/index.php
@@ -10,26 +10,26 @@ include '..\src\Exception.php';
 $url = 'http://www.huffingtonpost.com';
 
 // 1. return shares from Facebook and Twitter
-//$shares = new \SharesCounter\SharesCounter($url);
-//$counts = $shares->getShares([\SharesCounter\Networks::NETWORK_FACEBOOK, \SharesCounter\Networks::NETWORK_TWITTER]);
-//var_dump($counts);
+$shares = new \SharesCounter\SharesCounter($url);
+$counts = $shares->getShares([\SharesCounter\Networks::NETWORK_FACEBOOK, \SharesCounter\Networks::NETWORK_TWITTER]);
+var_dump($counts);
 
 // 2. return shares from all available networks
-//$shares = new \SharesCounter\SharesCounter($url);
-//$counts = $shares->getShares([]);
-//var_dump($counts);
+$shares = new \SharesCounter\SharesCounter($url);
+$counts = $shares->getShares([]);
+var_dump($counts);
 
 // 3. return shares from disabled by default network
-//$url = 'http://www.moy-rebenok.ru';
-//$shares = new \SharesCounter\SharesCounter($url);
-//$counts = $shares->getShares([\SharesCounter\Networks::NETWORK_VK, \SharesCounter\Networks::NETWORK_ODNOKLASSNIKI]);
-//var_dump($counts);
+$url = 'http://www.moy-rebenok.ru';
+$shares = new \SharesCounter\SharesCounter($url);
+$counts = $shares->getShares([\SharesCounter\Networks::NETWORK_VK, \SharesCounter\Networks::NETWORK_ODNOKLASSNIKI]);
+var_dump($counts);
 
 // 4. wykop.pl
-//$url = 'http://pokazywarka.pl/margaryna/';
-//$shares = new \SharesCounter\SharesCounter($url);
-//$counts = $shares->getShares([\SharesCounter\Networks::NETWORK_WYKOP]);
-//var_dump($counts);
+$url = 'http://pokazywarka.pl/margaryna/';
+$shares = new \SharesCounter\SharesCounter($url);
+$counts = $shares->getShares([\SharesCounter\Networks::NETWORK_WYKOP]);
+var_dump($counts);
 
 // 4. helper method - return list of available networks
 $networks = new \SharesCounter\Networks();
